c651bbc087bebe79c6f98399c36b8951be1e76a7
Shreevatsa MS <shreevatsa.dush@gmail.com>
2021-03-25 08:32:02 +0530

    Add files via upload

--- TwoDArray.java
+++ TwoDArray.java
@@ -1,74 +1,45 @@
-package TwoDArray;
-
-//Take the input of rows and column from the user
-//print the 2D array
-//create max and min variable of 2D array size
-//loop through the array
-//get the max[i][j]
-//get the min[i][j]
-//swap using simple swap
-//print array
+package twod;
 
 import java.util.Scanner;
 
 public class TwoDArray {
-	public static void main(String args[]) {
-		System.out.print("Enter 2D array size : ");
+
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int rows = sc.nextInt();
-		int columns = sc.nextInt();
-
-		System.out.println("Enter array elements : ");
-
-		int twoD[][] = new int[rows][columns];
-
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < columns; j++) {
-				twoD[i][j] = sc.nextInt();
+		System.out.println("Enter the array size");
+		int s = sc.nextInt();
+		System.out.println("Enter the elements");
+		int[][] arr = new int[s][s];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				arr[i][j] = sc.nextInt();
 			}
+
 		}
-		System.out.print("\nData you entered : \n");
-		for (int[] x : twoD) {
-			for (int y : x) {
-				System.out.print(y + "        ");
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				System.out.print(arr[i][j] + " ");
+
 			}
 			System.out.println();
+
 		}
+		System.out.println("\nThe diagonal elements are :");
+		diagonalElement(arr);
 
-		double max = twoD[0][0];
-		double min = twoD[0][0];
-		int maxIndex1 = 0;
-		int maxIndex2 = 0;
-		int minIndex1 = 0;
-		int minIndex2 = 0;
+	}
 
-		System.out.println("The matrix is : ");
-		for (int i = 0; i < twoD.length; i++) {
-			for (int j = 0; j < twoD[i].length; j++) {
+	private static void diagonalElement(int[][] arr) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (i == j) {
+					System.out.print(arr[i][j] + " ");
 
-				if (twoD[i][j] > max) {
-					max = twoD[i][j];
-
-					maxIndex1 = i;
-					maxIndex2 = j;
-				} else if (twoD[i][j] < min) {
-					min = twoD[i][j];
-
-					minIndex1 = i;
-					minIndex2 = j;
 				}
-			}
-		}
-		int temp = twoD[maxIndex1][maxIndex2];
-		twoD[maxIndex1][maxIndex2] = twoD[minIndex1][minIndex2];
-		twoD[minIndex1][minIndex2] = temp;
 
-		System.out.print("\nAfter Swapping: \n");
-		for (int[] x : twoD) {
-			for (int y : x) {
-				System.out.print(y + "        ");
 			}
-			System.out.println();
+
 		}
 
 	}
